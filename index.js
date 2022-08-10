797df0cdbd0d077aae0f8ba6640b481dc7c701ec
Yury Kovalev <ykovalev@redhat.com>
2022-08-10 10:09:49 +0200

    Initial spike for creating a badge

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import { makeBadge, ValidationError } from 'badge-maker'
+import { match } from 'assert';
+
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+const BADGE_LABEL = process.env.BADGE_LABEL || 'counter'
+const SOURCEGRAPH_QUERY = process.env.SOURCEGRAPH_QUERY
+
+if (!SOURCEGRAPH_QUERY) {
+    console.error("SOURCEGRAPH_QUERY env variable is not set.")
+    process.exit(1);
+}
+
+app.get('/', async function (req, res) {
+    const matchCount = await fetchMatchCount()
+    const format = {
+        label: BADGE_LABEL,
+        message: matchCount ? `${matchCount}` : 'n/a',
+        color: 'blue'
+    };
+
+    const svg = makeBadge(format);
+    res.status(200).send(svg);
+});
+
+app.listen(PORT, function () {
+    console.log('Server is running on port:', PORT);
+});
+
+async function fetchMatchCount() {
+    return await fetch('https://sourcegraph.com/.api/graphql', {
+      method: 'POST',
+  
+      headers: {
+        "Content-Type": "application/json"
+      },
+  
+      body: JSON.stringify({
+        query: `
+            query ($query: String!) {
+                search(query: $query, version: V2) {
+                    results {
+                        matchCount
+                    }
+                }
+            }
+        `,
+        variables: {
+            query: SOURCEGRAPH_QUERY
+        }
+      })
+    })
+    .then(x => x.json())
+    .then(x => x.data.search.results.matchCount)
+    .catch(err => console.log("Unable to fetch match count: ", err));
+}
